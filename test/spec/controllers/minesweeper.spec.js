17a77b78f00150a74fa77a1fc0995a6b8ac2558d
Shahar Talmi <shahar.talmi@gmail.com>
2014-06-27 00:35:14 +0300

    14th test

--- test/spec/controllers/minesweeper.spec.js
+++ test/spec/controllers/minesweeper.spec.js
@@ -4,6 +4,7 @@ describe('Controller: MinesweeperCtrl', function () {
 
   beforeEach(function () {
     module('minesweeperAppInternal');
+    module({$window: jasmine.createSpyObj('$window', ['alert'])});
     module(function ($provide) {
       $provide.factory('Minefield', function (gameState) {
         return function () {
@@ -28,4 +29,11 @@ describe('Controller: MinesweeperCtrl', function () {
   it('should have a minefield on the scope', function () {
     expect(scope.minefield.game.length).toBe(0);
   });
+
+  it('should alert that you lost', inject(function (gameState, $window) {
+    scope.$apply(function () {
+      scope.minefield.state = gameState.LOST;
+    });
+    expect($window.alert).toHaveBeenCalledWith('You Lost!');
+  }));
 });
