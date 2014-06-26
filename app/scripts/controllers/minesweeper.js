3e87818b14d70a541a032e5a8dda1ceec93a7908
Shahar Talmi <shahar.talmi@gmail.com>
2014-06-27 00:28:06 +0300

    13th test passed

--- app/scripts/controllers/minesweeper.js
+++ app/scripts/controllers/minesweeper.js
@@ -1,13 +1,6 @@
 'use strict';
 
 angular.module('minesweeperAppInternal')
-  .controller('MinesweeperCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'Bower',
-      'Grunt',
-      'Haml',
-      'Compass',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MinesweeperCtrl', function ($scope, Minefield) {
+    $scope.minefield = new Minefield(10, 10, 8);
   });
