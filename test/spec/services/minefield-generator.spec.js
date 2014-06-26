c12f7ade56fed18c86de417c27b1a63c83ba044d
Shahar Talmi <shahar.talmi@gmail.com>
2014-06-26 17:06:35 +0300

    2nd test

--- test/spec/services/minefield-generator.spec.js
+++ test/spec/services/minefield-generator.spec.js
@@ -15,14 +15,24 @@ describe('Service: MinefieldGenerator', function () {
     MinefieldGenerator = _MinefieldGenerator_;
   }));
 
-  it('should generate a field with given parameters', function () {
-    var mineField = [];
+  function mineField(arr) {
+    var field = [];
     for (var i = 0; i < 100; i++) {
-      mineField.push({mine: i < 8 ? true : false});
+      field.push({mine: arr.indexOf(i) !== -1 ? true : false});
     }
-    randomArr = [0, 1, 2, 3, 4, 5, 6, 7];
+    return field;
+  }
 
-    expect(MinefieldGenerator.create(10, 10, 8)).toEqual(mineField);
+  it('should generate a field with given parameters', function () {
+    var field = mineField([0, 1, 2, 3, 4, 5, 6, 7]);
+    randomArr = [0, 1, 2, 3, 4, 5, 6, 7];
+    expect(MinefieldGenerator.create(10, 10, 8)).toEqual(field);
+  });
+
+  it('should try to randomly select again if mine already exist', function () {
+    var field = mineField([0, 1, 2, 3, 4, 5, 6, 7]);
+    randomArr = [0, 1, 2, 3, 3, 4, 5, 6, 7];
+    expect(MinefieldGenerator.create(10, 10, 8)).toEqual(field);
   });
 
 });
