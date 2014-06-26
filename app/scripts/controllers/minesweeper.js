b78dd2a745dc198aa8abdc75481d514841d588b4
Shahar Talmi <shahar.talmi@gmail.com>
2014-06-27 00:37:52 +0300

    14th test passed

--- app/scripts/controllers/minesweeper.js
+++ app/scripts/controllers/minesweeper.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('minesweeperAppInternal')
-  .controller('MinesweeperCtrl', function ($scope, Minefield) {
+  .controller('MinesweeperCtrl', function ($scope, Minefield, gameState, $window) {
     $scope.minefield = new Minefield(10, 10, 8);
+
+    $scope.$watch('minefield.state', function (newValue) {
+      if (newValue === gameState.LOST) {
+        $window.alert('You Lost!');
+      }
+    });
   });
